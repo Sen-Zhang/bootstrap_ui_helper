124289e59aac73b0bbe75aec1206829f501bdba7
Sen-Zhang <senzhang1987@126.com>
2015-09-15 22:22:22 -0700

    add panel helper

--- app/helpers/alert_box_helper.rb
+++ app/helpers/alert_box_helper.rb
@@ -1,8 +1,9 @@
 module AlertBoxHelper
   include ActionView::Helpers
 
-  def alert_box(options={}, &block)
-    content     = options.delete(:content)
+  def alert_box(content_or_options=nil, options={}, &block)
+    content_or_options.is_a?(Hash) ? options = content_or_options : content = content_or_options
+
     dismissible = options.delete(:dismissible).present?
     klass       = options.delete(:class)
     type        = case options.delete(:type)
@@ -23,7 +24,7 @@ module AlertBoxHelper
     content_tag :div, options do
       if dismissible
         ("<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'" +
-          ">&times;</span></button>" + (content.presence || capture(&block))).html_safe
+          '>&times;</span></button>' + (content.presence || capture(&block))).html_safe
       else
         content.presence || capture(&block)
       end
---
+++ app/helpers/panel_helper.rb
@@ -0,0 +1,55 @@
+module PanelHelper
+  include ActionView::Helpers
+
+  def panel(content_or_options=nil, options={}, &block)
+    content_or_options.is_a?(Hash) ? options = content_or_options : content = content_or_options
+
+    heading = options.delete(:heading)
+    title   = options.delete(:title)
+    footer  = options.delete(:footer)
+    tag     = options.delete(:tag).try(:to_sym).presence || :div
+    klass   = options.delete(:class)
+    type    = case options.delete(:type)
+                when 'primary', :primary
+                  'panel-primary'
+                when 'info', :info
+                  'panel-info'
+                when 'success', :success
+                  'panel-success'
+                when 'warning', :warning
+                  'panel-warning'
+                when 'danger', :danger
+                  'panel-danger'
+                else
+                  'panel-default'
+              end
+
+    options.merge!({class: "panel #{type} #{klass}"})
+
+    content_tag tag, options do
+      (panel_header(heading, title) + panel_body(content, &block) + panel_footer(footer)).html_safe
+    end
+  end
+
+  private
+  def panel_header(heading, title)
+    return '' if heading.blank? && title.blank?
+
+    if title.present?
+      "<div class='panel-heading'><h3 class='panel-title'>#{title}</h3></div>"
+    else
+      "<div class='panel-heading'>#{heading}</div>"
+    end
+  end
+
+  def panel_body(content, &block)
+    content_tag :div, class: 'panel-body' do
+      content.presence || capture(&block)
+    end
+  end
+
+  def panel_footer(footer)
+    footer.present? ? "<div class='panel-footer'>#{footer}</div>" : ''
+  end
+
+end
