0b1b69ea9681e41e36adfe9a9d7f34d192fad12e
Sen-Zhang <senzhang1987@126.com>
2015-09-17 15:47:38 -0700

    add progress bar helper

---
+++ app/helpers/progress_bar_helper.rb
@@ -0,0 +1,62 @@
+module ProgressBarHelper
+
+  def progress_bar(bars_or_options=nil, options={})
+    bars = bars_or_options.is_a?(Hash) ? [bars_or_options] : bars_or_options
+
+    options[:class] = "progress #{options[:class]}"
+
+    content_tag :div, options do
+      bars.map { |bar| build_bar(bar) }.join('').html_safe
+    end
+
+  end
+
+  private
+  def process_options(options={})
+    percentage     = options.delete(:percentage) || 0
+    label          = options.delete(:label)
+    striped        = options.delete(:striped).presence
+    animated       = options.delete(:animated).presence
+    original_class = options.delete(:class) || ''
+    klass          = case options.delete(:type).try(:to_sym)
+                       when :info
+                         'progress-bar progress-bar-info'
+                       when :success
+                         'progress-bar progress-bar-success'
+                       when :warning
+                         'progress-bar progress-bar-warning'
+                       when :danger
+                         'progress-bar progress-bar-danger'
+                       else
+                         'progress-bar'
+                     end
+
+    unless label.is_a?(String)
+      label = label.is_a?(TrueClass) ? "#{percentage}%" : nil
+    end
+
+    if animated
+      klass += ' progress-bar-striped active'
+    elsif striped
+      klass += ' progress-bar-striped'
+    end
+
+    klass += original_class
+
+    [percentage, label, klass]
+  end
+
+  def build_bar(options={})
+    percentage, label, klass = process_options(options)
+    style                    = "width: #{percentage}%"
+
+    content_tag :div,
+                class: klass,
+                style: style,
+                role: 'progressbar',
+                aria: {valuemax: 100, valuemin: 0, valuenow: percentage} do
+      label.present? ? label : (content_tag :span, label, class: 'sr-only')
+    end
+  end
+
+end
--- lib/bootstrap_view_generator/version.rb
+++ lib/bootstrap_view_generator/version.rb
@@ -1,3 +1,3 @@
 module BootstrapViewGenerator
-  VERSION = '0.0.1'
+  VERSION = '0.1.0'
 end
