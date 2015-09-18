bcf206dade6311f98c53c0f4d24b6f81951e4d52
Sen-Zhang <senzhang1987@126.com>
2015-09-18 10:07:26 -0700

    refactor progress bar helper

--- app/helpers/progress_bar_helper.rb
+++ app/helpers/progress_bar_helper.rb
@@ -1,61 +1,72 @@
 module ProgressBarHelper
 
+  class ProgressBarCreator
+    include FormatHelper
+    include ActionView::Helpers
+
+    attr_accessor :options
+    attr_accessor :output_buffer
+
+    def initialize(options)
+      @options = options
+    end
+
+    def build
+      percentage, label, klass = process_progress_bar_options
+      style                    = "width: #{percentage}%"
+
+      content_tag :div,
+                  class: klass,
+                  style: style,
+                  role: 'progressbar',
+                  aria: {valuemax: 100, valuemin: 0, valuenow: percentage} do
+        label.present? ? label : (content_tag :span, label, class: 'sr-only')
+      end
+    end
+
+    private
+    def process_progress_bar_options
+      percentage     = options.delete(:percentage) || 0
+      label          = options.delete(:label)
+      striped        = options.delete(:striped).presence
+      animated       = options.delete(:animated).presence
+      original_class = options.delete(:class) || ''
+      klass          = case options.delete(:type).try(:to_sym)
+                         when :info
+                           'progress-bar progress-bar-info'
+                         when :success
+                           'progress-bar progress-bar-success'
+                         when :warning
+                           'progress-bar progress-bar-warning'
+                         when :danger
+                           'progress-bar progress-bar-danger'
+                         else
+                           'progress-bar'
+                       end
+
+      unless label.is_a?(String)
+        label = label.is_a?(TrueClass) ? "#{percentage}%" : nil
+      end
+
+      if animated
+        klass += ' progress-bar-striped active'
+      elsif striped
+        klass += ' progress-bar-striped'
+      end
+
+      klass += original_class
+
+      [percentage, label, klass]
+    end
+  end
+
   def progress_bar(bars_or_options=nil, options={})
     bars = bars_or_options.is_a?(Hash) ? [bars_or_options] : bars_or_options
 
     options[:class] = squeeze_n_strip("progress #{options[:class]}")
 
     content_tag :div, options do
-      bars.map { |bar| build_bar(bar) }.join('').html_safe
-    end
-
-  end
-
-  private
-  def process_progress_bar_options(options={})
-    percentage     = options.delete(:percentage) || 0
-    label          = options.delete(:label)
-    striped        = options.delete(:striped).presence
-    animated       = options.delete(:animated).presence
-    original_class = options.delete(:class) || ''
-    klass          = case options.delete(:type).try(:to_sym)
-                       when :info
-                         'progress-bar progress-bar-info'
-                       when :success
-                         'progress-bar progress-bar-success'
-                       when :warning
-                         'progress-bar progress-bar-warning'
-                       when :danger
-                         'progress-bar progress-bar-danger'
-                       else
-                         'progress-bar'
-                     end
-
-    unless label.is_a?(String)
-      label = label.is_a?(TrueClass) ? "#{percentage}%" : nil
-    end
-
-    if animated
-      klass += ' progress-bar-striped active'
-    elsif striped
-      klass += ' progress-bar-striped'
-    end
-
-    klass += original_class
-
-    [percentage, label, klass]
-  end
-
-  def build_bar(options={})
-    percentage, label, klass = process_progress_bar_options(options)
-    style                    = "width: #{percentage}%"
-
-    content_tag :div,
-                class: klass,
-                style: style,
-                role: 'progressbar',
-                aria: {valuemax: 100, valuemin: 0, valuenow: percentage} do
-      label.present? ? label : (content_tag :span, label, class: 'sr-only')
+      bars.map { |bar| ProgressBarCreator.new(bar).build }.join('').html_safe
     end
   end
 
