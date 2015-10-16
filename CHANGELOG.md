808fa30bb1baaaf8f133821d3669d2fdcad4d73c
Sen-Zhang <senzhang1987@126.com>
2015-10-16 16:23:50 -0700

    refactor panel_helper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,17 @@
-##v0.3.1
-- fix and improve dropdown helper
+# bootstrap_ui_helper Change Log
+
+All notable changes to this project will be documented in this file.
+
+bootstrap_ui_helper is in a 0.3.2 beta state. This means that its APIs and behavior are subject to breaking changes without deprecation notices.
+
+## [0.3.1]() (2015-10-16)
+
+Fixes:
+
+* Fix dropdown_helper
+
+## [0.3.2]() (2015-xx-xx)
+
+Changes:
+
+* Redo panel_helper
--- app/helpers/panel_helper.rb
+++ app/helpers/panel_helper.rb
@@ -1,78 +1,43 @@
 module PanelHelper
   include ActionView::Helpers
 
-  class PanelCreator
-    include FormatHelper
-    include ActionView::Helpers
+  def panel(options={}, &block)
+    tag  = options.delete(:tag).try(:to_sym) || :div
+    type = get_panel_type(options.delete(:type))
 
-    attr_accessor :content
-    attr_accessor :options
-    attr_accessor :block
-    attr_accessor :output_buffer
+    prepend_class(options, 'panel', type)
 
-    def initialize(content=nil, options, block)
-      @content = content
-      @options = options
-      @block   = block
-    end
+    content_tag tag, options, &block
+  end
 
-    def render
-      heading = options.delete(:heading)
-      title   = options.delete(:title)
-      footer  = options.delete(:footer)
-      tag     = options.delete(:tag).try(:to_sym).presence || :div
-      type    = get_panel_type(options.delete(:type))
+  %w(heading title body footer).each do |part|
+    define_method "panel_#{part}" do |content_or_options=nil, options={}, &block|
+      content_or_options.is_a?(Hash) ? options = content_or_options : content = content_or_options
 
-      prepend_class(options, 'panel', type)
+      tag = options.delete(:tag).try(:to_sym) || (part == 'title' ? :h3 : :div)
 
-      content_tag tag, options do
-        (panel_header(heading, title) + panel_body(content, block) +
-          panel_footer(footer)).html_safe
-      end
-    end
+      prepend_class(options, "panel-#{part}")
 
-    private
-    def panel_header(heading, title)
-      return '' if heading.blank? && title.blank?
-
-      if title.present?
-        "<div class='panel-heading'><h3 class='panel-title'>#{title}</h3></div>"
-      else
-        "<div class='panel-heading'>#{heading}</div>"
-      end
-    end
-
-    def panel_body(content, block)
-      content_tag :div, class: 'panel-body' do
-        content.presence || block
-      end
-    end
-
-    def panel_footer(footer)
-      footer.present? ? "<div class='panel-footer'>#{footer}</div>" : ''
-    end
-
-    def get_panel_type(type)
-      case type.try(:to_sym)
-      when :primary
-        'panel-primary'
-      when :info
-        'panel-info'
-      when :success
-        'panel-success'
-      when :warning
-        'panel-warning'
-      when :danger
-        'panel-danger'
-      else
-        'panel-default'
-      end
+      content_tag tag, content || capture(&block), options
     end
   end
 
-  def panel(content_or_options=nil, options={}, &block)
-    content_or_options.is_a?(Hash) ? options = content_or_options : content = content_or_options
+  private
 
-    PanelCreator.new(content, options, capture(&block)).render
+  def get_panel_type(type)
+    case type.try(:to_sym)
+    when :primary
+      'panel-primary'
+    when :info
+      'panel-info'
+    when :success
+      'panel-success'
+    when :warning
+      'panel-warning'
+    when :danger
+      'panel-danger'
+    else
+      'panel-default'
+    end
   end
 end
